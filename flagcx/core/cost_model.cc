dac7cf525fb2225447aea5094745dc80beff1ac6
mikethegoblin <mikegu0508@gmail.com>
2025-05-07 14:44:07 +0800

    enable algo time estimator when topology detection is enabled

--- flagcx/core/cost_model.cc
+++ flagcx/core/cost_model.cc
@@ -6,14 +6,21 @@ const float flagcxLatMap[FLAGCX_VENDOR_NUM][2] = {
     {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}, {0.0, 0.0}};
 
 flagcxResult_t flagcxAlgoTimeEstimator::getAlgoTime(float *time) {
-  float preHomoTime, heteroTime, postHomoTime;
-  INFO(FLAGCX_GRAPH, "COST_MODEL: getting time for prehomo funcs");
-  FLAGCXCHECK(getPreHomoAlgoTime(&preHomoTime));
-  INFO(FLAGCX_GRAPH, "COST_MODEL: getting time for hetero funcs");
-  FLAGCXCHECK(getHeteroAlgoTime(&heteroTime));
-  INFO(FLAGCX_GRAPH, "COST_MODEL: getting time for posthomo funcs");
-  FLAGCXCHECK(getPostHomoAlgoTime(&postHomoTime));
-  *time = preHomoTime + heteroTime + postHomoTime;
+  const char *enableTopoDetect = flagcxGetEnv("FLAGCX_ENABLE_TOPO_DETECT");
+  const char *interServerTopoFile =
+      flagcxGetEnv("FLAGCX_INTERSERVER_ROUTE_FILE");
+  if (enableTopoDetect && interServerTopoFile &&
+      strcmp(enableTopoDetect, "TRUE") == 0) {
+    // algo time estimator depends on cluster level topology detection
+    float preHomoTime, heteroTime, postHomoTime;
+    INFO(FLAGCX_GRAPH, "COST_MODEL: getting time for prehomo funcs");
+    FLAGCXCHECK(getPreHomoAlgoTime(&preHomoTime));
+    INFO(FLAGCX_GRAPH, "COST_MODEL: getting time for hetero funcs");
+    FLAGCXCHECK(getHeteroAlgoTime(&heteroTime));
+    INFO(FLAGCX_GRAPH, "COST_MODEL: getting time for posthomo funcs");
+    FLAGCXCHECK(getPostHomoAlgoTime(&postHomoTime));
+    *time = preHomoTime + heteroTime + postHomoTime;
+  }
   return flagcxSuccess;
 }
 
