fa99d932c388b6c2fe2b9e18f6b94b6cef3c0705
mikethegoblin <mikegu0508@gmail.com>
2025-10-27 15:15:55 +0800

    add tuner config for mccl

--- flagcx/adaptor/tuner/tuner_util.h
+++ flagcx/adaptor/tuner/tuner_util.h
@@ -2,23 +2,26 @@
 #define FLAGCX_TUNER_UTIL_H_
 
 #include "tuner.h" // struct flagcxEnvConfig
-#include <vector>
 #include <string>
+#include <vector>
 
-// This is a demonstration function that provide a way to load all config list for a specific GPU.
+// This is a demonstration function that provide a way to load all config list
+// for a specific GPU.
 
 struct EnvVar {
-    std::string name;
-    std::vector<std::string> choices;
-    std::string defaultValue;
-    EnvVar(std::string n="") : name(std::move(n)) {}
-    EnvVar(std::string n, std::vector<std::string> c, std::string d = "")
+  std::string name;
+  std::vector<std::string> choices;
+  std::string defaultValue;
+  EnvVar(std::string n = "") : name(std::move(n)) {}
+  EnvVar(std::string n, std::vector<std::string> c, std::string d = "")
       : name(std::move(n)), choices(std::move(c)), defaultValue(std::move(d)) {}
 };
 
 flagcxResult_t generateCandidate(std::vector<struct flagcxEnvConfig> &cfgList);
 static void safeStrCopy(char *dst, size_t dstSize, const std::string &src);
 
+extern std::vector<EnvVar> ncclTunerVars;
+extern std::vector<EnvVar> mcclTunerVars;
 extern std::vector<EnvVar> vars;
 
 #endif // end include guard
